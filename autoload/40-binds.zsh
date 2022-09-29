20b8b9a957365079697143a6638c5b355688093e
System.rat <system.rodent@gmail.com>
2022-09-29 12:42:54 +0200

    man page invocation widget
    Invoke the man page on the first command using Alt+M

--- autoload/40-binds.zsh
+++ autoload/40-binds.zsh
@@ -3,3 +3,6 @@ bindkey "^[[1;5C" forward-word
 bindkey "^H" backward-delete-word
 bindkey "^[[3~" delete-char
 bindkey "^[[3;5~" delete-word
+
+zle -N __rat_open_in_man
+bindkey "^[m" __rat_open_in_man
