ba8b94deae9334ef87c670536b160cf2d9971135
System.rat <system.rodent@gmail.com>
2022-09-29 13:53:27 +0200

    Timing function to RPROMPT
    - Execution time of a command displayed on the right side
    - Some QoL changes to the man widget

--- autoload/70-prompt.zsh
+++ autoload/70-prompt.zsh
@@ -1,34 +1,28 @@
-__rat_col() {
-  local arg=$1
-  local fcol=$2
-  local bcol=$3
-  local res=""
-
-  res+="%F{$fcol}"
-  res+="%K{$bcol}"
-  res+=$arg
-  res+="%k"
-  res+="%f"
-
-  echo $res
-}
-
-__rat_fa() {
-  echo $(__rat_col "" $1 $2)
-}
-
-__rat_ba() {
-  echo $(__rat_col "" $1 $2)
-}
-
-#PROMPT=$(__rat_col "%B%n%M: " cyan black)
-#PROMPT+=$(__rat_col "%~ " blue black)
-#PROMPT+="%(?.."$(__rat_col " " red black)$(__rat_ba red black)$(__rat_col "ERR %?" white red)$(__rat_fa red black)")"
-#PROMPT+="$(__rat_fa black default)%b "
-
 # TODO: FIX THIS ABHORRENT MESS
 
 PROMPT="%B%K{black}%(!.%F{red}.%F{cyan})%n@%M%f%b:%B%F{blue}%~%f %(?..%F{red}%K{black}%k%f%F{white}%K{red}ERR %?%k%f%F{red}%K{black}%k%f%K{black} %k)%k%F{black}%f%b "
 PROMPT2="%B%K{black}%F{cyan}%_%f%k%F{black}%f%b "
 PROMPT3="%B%K{black}%F{cyan}select answer: %f%k%F{black}%f%b "
 PROMPT4="%B%K{black}%F{red}+%N: %i%f%k%F{black}%f%b "
+
+preexec() {
+  __rat_executing=1
+  __rat_time=$(date +%s%3N)
+}
+
+precmd() {
+  if [[ $__rat_executing == 1 ]] && [[ -n $__rat_time ]]; then
+    local t=$(( $(date +%s%3N) - $__rat_time ))
+
+    if (( $t > 1000 )); then
+      RPROMPT="%F{black}($(( $t / 1000 )).$(( $t % 1000 ))s)%f"
+    else
+      RPROMPT=""
+    fi
+  else
+    RPROMPT=""
+  fi
+
+  __rat_executing=0
+}
+
