8594918aa949e2e5bf7b4af7fcd0d5799b07edb2
System.rat <system.rodent@gmail.com>
2022-10-11 16:18:21 +0200

    Prompt code cleanup
    Well it's a lot more clean than before at least

--- autoload/70-prompt.zsh
+++ autoload/70-prompt.zsh
@@ -1,11 +1,132 @@
-# TODO: FIX THIS ABHORRENT MESS
+: ${__rat_fore:="cyan"}
+: ${__rat_back:="black"}
+local __ratp="PROMPT"
+
+function __ratpu() {
+  : ${(P)__ratp::="${(P)__ratp}"$@}
+}
+
+function __ratcol() {
+  case $1 in
+    fg)
+      __ratpu "%F{"$2"}"
+      ;;
+    bg)
+      __ratpu "%K{"$2"}"
+      ;;
+    *)
+      echo __ratcol: $(ansi bold red)No such identifier $1
+      ;;
+  esac
+}
+
+function __ratcolwr() {
+  __ratcol $1 $2
+  shift; shift
+  __ratpu $@
+  __ratcol fg $__rat_fore
+  __ratcol bg $__rat_back
+}
+
+function __rat_begin_prompt() {
+  alias fa="__ratpu "
+  alias ba="__ratpu "
+  alias fore="__ratcol fg"
+  alias forer="__ratpu '%f'"
+  alias wfore="__ratcolwr fg"
+  alias wback="__ratcolwr bg"
+  alias back="__ratcol bg"
+  alias backr="__ratpu '%k'"
+  alias iden="__ratpu"
+  alias bold="__ratpu '%B'"
+  alias boldr="__ratpu '%b'"
+  alias end_prompt="__ratpu '%f%k%b'"
+}
+
+function __rat_end_prompt() {
+  unalias fa ba fore forer wfore wback back backr iden end_prompt bold boldr
+}
+
+__rat_begin_prompt
+
+function __rat_build_error() {
+  iden "%(?.."
+  fore red; ba
+  fore white
+  back red
+  iden "ERR %?"
+  fore red
+  back $__rat_back
+  fa
+  fore $__rat_fore
+  iden " )"
+}
+
+function __rat_build_prompt1 {
+  PROMPT=""
+  __ratp="PROMPT"
+  fore $__rat_fore
+  back $__rat_back
+
+  bold
+  iden "%n@%M"
+  boldr
+  wfore white ":"
+  bold
+  wfore blue "%~ "
+
+  __rat_build_error
+
+  fore $__rat_back
+  backr
+  fa
+  end_prompt
+  iden " "
+}
+
+function __rat_simple_prompt {
+  local _fore=$1
+  local _back=$2
+  shift; shift
+  fore $_fore
+  back $_back
+  bold
+
+  iden "$@"
+  fore $_back
+  backr
+  fa
+  iden " "
+  end_prompt
+}
+
+function __rat_build_prompt2 {
+  PROMPT2=""
+  __ratp="PROMPT2"
+  __rat_simple_prompt $__rat_fore $__rat_back '%_ '
+}
+
+function __rat_build_prompt3 {
+  PROMPT3=""
+  __ratp="PROMPT3"
+
+  __rat_simple_prompt $__rat_fore $__rat_back 'select answer: '
+}
+
+function __rat_build_prompt4 {
+  PROMPT4=""
+  __ratp="PROMPT4"
+  __rat_simple_prompt red white '+%N: %i'
+}
 
 setopt promptsubst
 
-PROMPT="%B%K{black}%(!.%F{red}.%F{cyan})%n@%M%f%b:%B%F{blue}%~%f %(?..%F{red}%K{black}%k%f%F{white}%K{red}ERR %?%k%f%F{red}%K{black}%k%f%K{black} %k)%k%F{black}%f%b "
-PROMPT2="%B%K{black}%F{cyan}%_%f%k%F{black}%f%b "
-PROMPT3="%B%K{black}%F{cyan}select answer: %f%k%F{black}%f%b "
-PROMPT4="%B%K{black}%F{red}+%N: %i%f%k%F{black}%f%b "
+__rat_build_prompt1
+__rat_build_prompt2
+__rat_build_prompt3
+__rat_build_prompt4
+
+__rat_end_prompt
 
 preexec() {
   __rat_executing=1
