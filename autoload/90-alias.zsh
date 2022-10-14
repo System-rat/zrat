6b2bd3c89036256c42aa649ec3027fe30fee708f
System.rat <system.rodent@gmail.com>
2022-10-14 16:41:17 +0200

    Rat menu fixes
    - Fixes for some rat menu stuff
    - Additional ansi outputs
    - Added some nice-to-have aliases

--- autoload/90-alias.zsh
+++ autoload/90-alias.zsh
@@ -2,6 +2,10 @@
 (( $+commands[lsd] )) && alias ll="lsd -alhF" || alias ll="ls -alhF"
 alias help="run-help"
 (( $+commands[notify-send] )) && alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+if (( $+commands[fdfind] )); then
+  alias fd="fdfind"
+  alias find="fdfind"
+fi
 
 # git
 alias gis='git status'
@@ -15,4 +19,4 @@ alias gipu='git push'
 alias gil='git log'
 alias REE_STOP_FUCKING_AROUND='git stash && git pull && git stash pop'
 alias reload='source ~/.zshrc'
-alias reload_hard='exec zsh --login'
+alias reload_hard='exec -l zsh'
