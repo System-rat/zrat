820704270e45a11599ffddad8b2f4853fa9a0ed0
System.rat <system.rodent@gmail.com>
2024-12-11 17:11:22 +0100

    Add aliases
    Remove the old re-sourcing alias since it doesn't work as expected,
    and add some extra ones along with fixing old ones.

--- autoload/90-alias.zsh
+++ autoload/90-alias.zsh
@@ -1,5 +1,5 @@
 (( $+commands[lsd] )) && alias ls="lsd" || alias ls="ls --color=auto"
-(( $+commands[lsd] )) && alias ll="lsd -alhF" || alias ll="ls -alhF"
+(( $+commands[lsd] )) && alias ll="lsd -AlhF" || alias ll="ls -AlhF"
 alias help="run-help"
 (( $+commands[notify-send] )) && alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 if (( $+commands[fdfind] )); then
@@ -22,8 +22,9 @@ fi
 (( $+commands[batcat] )) && alias cat="batcat"
 
 # git
-alias gis='git status'
+alias gis='git status -sb'
 alias gid='git diff --word-diff=color'
+alias gids='git diff --word-diff=color --staged'
 alias gidt='git difftool'
 alias gif='git fetch'
 alias gic='git commit'
@@ -36,7 +37,20 @@ alias gil='git log'
 alias giw='git switch'
 alias REE_STOP_FUCKING_AROUND='git stash && git pull && git stash pop'
 alias I_SAID_PUSH_THE_DAMN_BRANCH='git push --force'
-alias reload='source ~/.zshrc'
-alias reload_hard='exec -l zsh'
+
+# Nix specific
+
+if (( $+commands[nix] )); then
+  alias nib="nix-build --log-format bar-with-logs"
+  alias nis="nix-shell --log-format bar-with-logs"
+fi
+
+if (( $+commands[nixos-rebuild] )); then
+  alias nos-rs="nixos-rebuild switch"
+  alias nos-rsu="nixos-rebuild switch --upgrade"
+fi
+
+# Extras
+alias reload='exec -l zsh'
 alias tarit='tar --zstd -cf $(read -e "?File name: ")-$(date +%Y-%m-%d--%H-%M).tar.zst'
 alias zipit='zip -r $(read -e "?File name: ")-$(date +%Y-%m-%d--%H-%M).zip'
